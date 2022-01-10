aa2d32dcbd07dfbedff9f80f4e7a8c25c2f009c5
Nahua <kangnahua@gmail.com>
2022-01-10 15:50:38 +0100

    Add camera for separating map and player layers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod camera;
 mod map;
 mod map_builder;
 mod player;
@@ -6,6 +7,9 @@ mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const SCREEN_WIDTH: i32 = 80;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub use crate::camera::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
@@ -14,6 +18,7 @@ mod prelude {
 use prelude::*;
 
 struct State {
+    camera: Camera,
     map: Map,
     player: Player,
 }
@@ -24,6 +29,7 @@ impl State {
         let map_builder = MapBuilder::new(&mut rng);
 
         Self {
+            camera: Camera::new(map_builder.player_start),
             map: map_builder.map,
             player: Player::new(map_builder.player_start),
         }
@@ -32,17 +38,29 @@ impl State {
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
+        // Set active console to map layer
+        ctx.set_active_console(0);
         ctx.cls();
-        self.player.update(ctx, &self.map);
-        self.map.render(ctx);
-        self.player.render(ctx);
+
+        // Set active console to player layer
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.map.render(ctx, &self.camera);
+        self.player.render(ctx, &self.camera);
     }
 }
 
 fn main() -> BError {
-    let ctx = BTermBuilder::simple80x50()
+    let ctx = BTermBuilder::new()
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(ctx, State::new())
