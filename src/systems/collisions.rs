15008d25427ab63758685b86be898cf85a3ce7a6
nahuakang <kangnahua@gmail.com>
2022-02-27 14:52:51 +0100

    Create collision

---
+++ src/systems/collisions.rs
@@ -0,0 +1,21 @@
+use crate::prelude::*;
+
+// `CommandBuffer` is a special container to insert instructions for
+// Legion to perform after the system is finished. We use it to remove entities from the game.
+#[system]
+#[read_component(Point)]
+#[read_component(Player)]
+#[read_component(Enemy)]
+pub fn collisions(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
+    let mut player_pos = Point::zero();
+    let mut players = <&Point>::query().filter(component::<Player>());
+    players.iter(ecs).for_each(|pos| player_pos = *pos);
+
+    let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+    enemies
+        .iter(ecs)
+        .filter(|(_, pos)| **pos == player_pos)
+        .for_each(|(entity, _)| {
+            commands.remove(*entity);
+        });
+}
